de367d228c88710a842ed0bd27e61d526943a075
bemardev <55793274+bemardev@users.noreply.github.com>
2026-03-10 18:31:13 +0100

    Update create-release-branch.sh

--- scripts/create-release-branch.sh
+++ scripts/create-release-branch.sh
@@ -420,6 +420,7 @@ debug_commits "$source_branch" "$target_branch" "$release_branch"
 echo "jq : main 2"
 echo "$pr_data"
 #json=$(echo "$pr_data" | jq reverse)
+json=$(echo "")
 jq reverse <<<"$pr_data" > "$json"
 
 echo "jq : main 2 OK"
